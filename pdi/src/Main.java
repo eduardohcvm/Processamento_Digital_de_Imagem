3b261ce73e17f6f1270c87b2ba02af0bb817e495
Eduardo Henrique Calado Vieira de Melo <57454262+eduardohcvm@users.noreply.github.com>
2024-03-05 07:27:36 -0300

    Update Main.java
    segundo commit

--- pdi/src/Main.java
+++ pdi/src/Main.java
@@ -1,14 +1,15 @@
+package app;
+
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 
+// ctrl + alt + s -> import e ativar os dois de java
+
 public class Main {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\eduar\\OneDrive\\Desktop\\pikachu.jpg");
+        File file = new File("C:\\Users\\autologon\\Desktop\\defante.jpg");
 
         try {
             BufferedImage imagem = ImageIO.read(file);
@@ -18,6 +19,8 @@ public class Main {
             System.out.println("Altura: " + altura);
             int pegarRPG = imagem.getRGB(largura/2,altura/2);
             System.out.println("pixel central cor: " + pegarRPG);
+            int qntPixel = altura * largura;
+            System.out.println("Quantidade de pixels: " + qntPixel);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
