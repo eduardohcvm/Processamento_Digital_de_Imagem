66f266ee60692b42d8a523d2375007424553fe04
Eduardo Henrique Calado Vieira de Melo <57454262+eduardohcvm@users.noreply.github.com>
2024-03-05 09:02:10 -0300

    Update Main.java

--- untitled/src/app/Main.java
+++ untitled/src/app/Main.java
@@ -9,8 +9,10 @@ import java.io.IOException;
 
 
 public class Main {
+
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\autologon\\Desktop\\defante.jpg");
+
+        File file = new File("C:\\Users\\autologon\\Desktop\\pokemon.jpg");
 
         BufferedImage imagem = null;
 
@@ -29,12 +31,34 @@ public class Main {
             int pegarRPG = imagem.getRGB(largura/2,altura/2);
             System.out.println("pixel central cor: " + pegarRPG);
 
-            salvarImagem(imagem,"jpg","saidaDefante.jpg");
+            mudarImagem(imagem, altura, largura);
+
+
+            salvarImagem(imagem,"jpg","pokemon.jpg");
+
 
 
             display(imagem);
         }
 
+    }
+
+    private static void mudarImagem(BufferedImage imagem, int altura, int largura) {
+        Color cor1 = new Color(0,0,255); // azul
+        Color cor2 = new Color(0,255,0);
+        Color cor3 = new Color(255,0,0);
+
+        imagem.setRGB(0,0, cor1.getRGB());
+        imagem.setRGB(altura /2, largura /2,cor2.getRGB());
+        imagem.setRGB(altura -1, largura -1,cor3.getRGB());
+        for (int i = 0; i < altura; i++){
+            for (int j = 0; j < largura; j++){
+                if (imagem.getRGB(i,j) != -1){
+                    imagem.setRGB(i,j,imagem.getRGB(i,j) / 2);
+                    System.out.println(imagem.getRGB(i,j));
+                }
+            }
+        }
     }
 
     private static void salvarImagem(BufferedImage imagem, String formato, String nomeImagem) {
@@ -60,4 +84,4 @@ public class Main {
         frame.pack();
         frame.setVisible(true);
     }
-}
+    }
