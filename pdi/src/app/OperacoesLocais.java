d56fc21fc75f90406f4158dd389b94de43e6f60f
Eduardo Henrique Calado Vieira de Melo <57454262+eduardohcvm@users.noreply.github.com>
2024-04-23 08:55:11 -0300

    Update OperacoesLocais.java
    convolucao

--- pdi/src/app/OperacoesLocais.java
+++ pdi/src/app/OperacoesLocais.java
@@ -114,4 +114,47 @@ public class OperacoesLocais {
         }
         return imgSaida;
     }
+
+    public static BufferedImage convolucao(BufferedImage imagem, int[] kernel){
+        BufferedImage imgSaida = new BufferedImage(imagem.getWidth(), imagem.getHeight(), imagem.getType());
+        if (imagem != null){
+
+            for (int h = 0; h < imagem.getHeight(); h++) {
+                for (int w = 0; w < imagem.getWidth(); w++) {
+                    if (w == 0 || h == 0 || w == imagem.getWidth() - 1 || h == imagem.getHeight() - 1){
+                        int rgb = imagem.getRGB(w,h);
+                        imgSaida.setRGB(w,h,rgb);
+                        continue;
+                    }
+                    int[] vizinhanca = new int[9];
+                    int count = 0;
+
+                    for (int i = -1; i <= 1 ; i++) {
+                        for (int j = -1; j <= 1 ; j++) {
+                            Color cor = new Color(imagem.getRGB(w+j,h+i));
+                            vizinhanca[count++] =  (cor.getRed());
+                        }
+                    }
+                    int resultado = 0;
+                    for (int a = 0; a < 9; a++) {
+                        resultado += vizinhanca[a] * kernel[a];
+                    }
+
+                    if (resultado > 255) resultado = 255;
+                    else if (resultado < 0) {
+                        resultado = 0;
+                    }
+
+                    Color novaCor = new Color(resultado,resultado,resultado);
+
+                    imgSaida.setRGB(w,h,novaCor.getRGB());
+                }
+
+                ManipulaImagem.salvarImagem(imgSaida, "jpg", new File("C:\\Users\\autologon\\Desktop\\saidaKernel.jpg"));
+
+                //display(imagem);
+            }
+        }
+        return imgSaida;
+    }
 }
