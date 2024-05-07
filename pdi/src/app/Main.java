2ef147a6ea359b60da62498b4958b8799bc54b76
Eduardo Henrique <eduardohcvm@gmail.com>
2024-05-06 21:59:19 -0300

    matriz media e mediana para qualquer matriz de entrada (impar)

--- pdi/src/app/Main.java
+++ pdi/src/app/Main.java
@@ -14,11 +14,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File file = new File("C:\\Users\\eduar\\OneDrive\\Desktop\\imagem.jpg");
+        File file = new File("C:\\Users\\eduar\\OneDrive\\Desktop\\bolsa.jpg");
         BufferedImage imagem = null;
          int [] kernelHorizontal = {-1,-2,-1,0,0,0,1,2,1};
         int [] kernelVertical = {-1,0,1,-2,0,2,-1,0,-1};
         int[] kernelLinhasOeste = { 1,1,-1,1,-2,-1,1,1,1};
+        int [] Laplaciano = {0,-1,0,-1,4,-1,0,-1,0};
         
         try {imagem = ImageIO.read(file);}
         catch (IOException e) {
@@ -26,23 +27,30 @@ public class Main {
         }
         app.OperacaoPontual.buscarDados(imagem);
 
-        //ManipularImagem.mudarRGB(imagem, imagem.getHeight(), imagem.getWidth());
-        //ManipularImagem.filtroNegativo(imagem, imagem.getHeight(), imagem.getWidth());
-        //ManipularImagem.filtroCinza(imagem, imagem.getHeight(), imagem.getWidth(), "green");
-        //ManipularImagem.Binarizacao(imagem, imagem.getHeight(), imagem.getWidth(),200);
-        ManipularImagem.Tonalizacao(imagem, imagem.getHeight(), imagem.getWidth(),"blue",200);
-       // ManipularImagem.brilhoADD(imagem, imagem.getHeight(), imagem.getWidth(),100);
-       // ManipularImagem.brilhoMulti(imagem, imagem.getHeight(), imagem.getWidth(),0.5f);
-
-        //conversaoYIQ(imagem);
+//        ManipularImagem.mudarRGB(imagem, imagem.getHeight(), imagem.getWidth());
+//        ManipularImagem.filtroNegativo(imagem, imagem.getHeight(), imagem.getWidth());
+//        ManipularImagem.filtroCinza(imagem, imagem.getHeight(), imagem.getWidth(), "green");
+//        ManipularImagem.Binarizacao(imagem, imagem.getHeight(), imagem.getWidth(),200);
+//        ManipularImagem.Tonalizacao(imagem, imagem.getHeight(), imagem.getWidth(),"blue",200);
+//        ManipularImagem.brilhoADD(imagem, imagem.getHeight(), imagem.getWidth(),100);
+//        ManipularImagem.brilhoMulti(imagem, imagem.getHeight(), imagem.getWidth(),0.5f);
+//
+//        conversaoYIQ(imagem);
 //        conversaoYIQADD(imagem, 75.89);
 //        conversaoYIQMULTI(imagem, 1.5);
-        OperacoesLocais.mediaMatriz3x3(imagem);
-        OperacoesLocais.convolucao(imagem, kernelVertical);
+          OperacoesLocais.medianaMatriz3x3(imagem);
+          OperacoesLocais.mediaMatriz3x3(imagem);
+//        OperacoesLocais.convolucao(imagem, kernelHorizontal);
+//        OperacoesLocais.convolucao(imagem, kernelVertical);
+//        OperacoesLocais.convolucao(imagem, kernelLinhasOeste);
+//        OperacoesLocais.convolucao(imagem, Laplaciano);
+//        OperacoesLocais.medianaMatriz3x3(imagem);
+          OperacoesLocais.mediaMatrizDefinida(imagem, 3);
+          OperacoesLocais.medianaMatrizDefinida(imagem, 3);
     }
 
     private static void conversaoYIQ(BufferedImage imagem) {
-        double matriz[][][] = new double[imagem.getHeight()][imagem.getWidth()][3];
+        double[][][] matriz = new double[imagem.getHeight()][imagem.getWidth()][3];
         matriz = ConverterParaYIQ.rgbParaYiq(imagem);
         System.out.println(ConverterParaYIQ.retornarValorQ(matriz, 200,200));
         BufferedImage image;
@@ -50,7 +58,7 @@ public class Main {
         OperacaoPontual.buscarDados(image);
     }
     private static void conversaoYIQADD(BufferedImage imagem, double tonalidade) {
-        double matriz[][][] = new double[imagem.getHeight()][imagem.getWidth()][3];
+        double[][][] matriz = new double[imagem.getHeight()][imagem.getWidth()][3];
         matriz = ConverterParaYIQ.rgbParaYiq(imagem);
         ManipularEmYIQ.brilhoADDemYIQ(matriz,tonalidade);
         System.out.println(ConverterParaYIQ.retornarValorQ(matriz, 200,200));
@@ -59,7 +67,7 @@ public class Main {
         OperacaoPontual.buscarDados(image);
     }
     private static void conversaoYIQMULTI(BufferedImage imagem, double tonalidade) {
-        double matriz[][][] = new double[imagem.getHeight()][imagem.getWidth()][3];
+        double[][][] matriz = new double[imagem.getHeight()][imagem.getWidth()][3];
         matriz = ConverterParaYIQ.rgbParaYiq(imagem);
         ManipularEmYIQ.brilhoMULTIemYIQ(matriz,tonalidade);
         System.out.println(ConverterParaYIQ.retornarValorQ(matriz, 200,200));
--- pdi/src/app/OperacoesLocais.java
+++ pdi/src/app/OperacoesLocais.java
@@ -24,6 +24,7 @@ public class OperacoesLocais {
                     }
                     int[] vizinhanca = new int[9];
                     int count = 0;
+
                     for (int i = -1; i <= 1; i++) {
                         for (int j = -1; j <= 1; j++) {
                             Color cor = new Color(imagem.getRGB(w + j, h + i));
@@ -37,7 +38,7 @@ public class OperacoesLocais {
             }
         }
 
-        salvarImagem(imgSaida,"jpg","MatrixMediaSaida.jpg");
+        salvarImagem(imgSaida,"jpg","MatrixMediaSaida.png");
         display(imgSaida);
         return imgSaida;
     }
@@ -69,7 +70,7 @@ public class OperacoesLocais {
 
             }
         }
-        salvarImagem(imgSaida,"jpg","medianaMatrizSaida.jpg");
+        salvarImagem(imgSaida,"jpg","medianaMatrizSaida.png");
         display(imgSaida);
         return imgSaida;
     }
@@ -82,82 +83,76 @@ public class OperacoesLocais {
      */
     public static BufferedImage multiplicacaoGaussiana(BufferedImage imagem) {
         BufferedImage imgSaida = new BufferedImage(imagem.getWidth(), imagem.getHeight(), imagem.getType());
-        if (imagem != null) {
-
-            for (int h = 0; h < imagem.getHeight(); h++) {
-                for (int w = 0; w < imagem.getWidth(); w++) {
-                    if (w == 0 || h == 0 || w == imagem.getWidth() - 1 || h == imagem.getHeight() - 1) {
-                        int rgb = imagem.getRGB(w, h);
-                        imgSaida.setRGB(w, h, rgb);
-                        continue;
+        for (int h = 0; h < imagem.getHeight(); h++) {
+            for (int w = 0; w < imagem.getWidth(); w++) {
+                if (w == 0 || h == 0 || w == imagem.getWidth() - 1 || h == imagem.getHeight() - 1) {
+                    int rgb = imagem.getRGB(w, h);
+                    imgSaida.setRGB(w, h, rgb);
+                    continue;
+                }
+                int[] vizinhanca = new int[9];
+                int count = 0;
+                double[][] matrizGau = {
+                        {0.0625, 0.125, 0.0625},
+                        {0.125, 0.25, 0.125},
+                        {0.0625, 0.125, 0.0625}
+                };
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        Color cor = new Color(imagem.getRGB(w + j, h + i));
+                        vizinhanca[count++] = (int) (cor.getRed() * matrizGau[i + 1][j + 1]);
                     }
-                    int[] vizinhanca = new int[9];
-                    int count = 0;
-                    double[][] matrizGau = {
-                            {0.0625, 0.125, 0.0625},
-                            {0.125, 0.25, 0.125},
-                            {0.0625, 0.125, 0.0625}
-                    };
-                    for (int i = -1; i <= 1; i++) {
-                        for (int j = -1; j <= 1; j++) {
-                            Color cor = new Color(imagem.getRGB(w + j, h + i));
-                            vizinhanca[count++] = (int) (cor.getRed() * matrizGau[i + 1][j + 1]);
-                        }
-                    }
-
-
-                    int media = Arrays.stream(vizinhanca).sum();
-                    Color novaCor = new Color(media, media, media);
-
-                    imgSaida.setRGB(w, h, novaCor.getRGB());
                 }
 
+
+                int media = Arrays.stream(vizinhanca).sum();
+                Color novaCor = new Color(media, media, media);
+
+                imgSaida.setRGB(w, h, novaCor.getRGB());
             }
+
         }
-        salvarImagem(imgSaida,"jpg","GaussianaSaida.jpg");
+        salvarImagem(imgSaida,"jpg","GaussianaSaida.png");
         display(imgSaida);
         return imgSaida;
     }
 
     public static BufferedImage convolucao(BufferedImage imagem, int[] kernel) {
         BufferedImage imgSaida = new BufferedImage(imagem.getWidth(), imagem.getHeight(), imagem.getType());
-        if (imagem != null) {
+        for (int h = 0; h < imagem.getHeight(); h++) {
+            for (int w = 0; w < imagem.getWidth(); w++) {
+                if (w == 0 || h == 0 || w == imagem.getWidth() - 1 || h == imagem.getHeight() - 1) {
+                    int rgb = imagem.getRGB(w, h);
+                    imgSaida.setRGB(w, h, rgb);
+                    continue;
+                }
+                int[] vizinhanca = new int[9];
+                int count = 0;
 
-            for (int h = 0; h < imagem.getHeight(); h++) {
-                for (int w = 0; w < imagem.getWidth(); w++) {
-                    if (w == 0 || h == 0 || w == imagem.getWidth() - 1 || h == imagem.getHeight() - 1) {
-                        int rgb = imagem.getRGB(w, h);
-                        imgSaida.setRGB(w, h, rgb);
-                        continue;
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        Color cor = new Color(imagem.getRGB(w + j, h + i));
+                        vizinhanca[count++] = (cor.getRed());
                     }
-                    int[] vizinhanca = new int[9];
-                    int count = 0;
-
-                    for (int i = -1; i <= 1; i++) {
-                        for (int j = -1; j <= 1; j++) {
-                            Color cor = new Color(imagem.getRGB(w + j, h + i));
-                            vizinhanca[count++] = (cor.getRed());
-                        }
-                    }
-                    int resultado = 0;
-                    for (int a = 0; a < 9; a++) {
-                        resultado += vizinhanca[a] * kernel[a];
-                    }
-
-                    if (resultado > 255) resultado = 255;
-                    else if (resultado < 0) {
-                        resultado = 0;
-                    }
-
-                    Color novaCor = new Color(resultado, resultado, resultado);
-
-                    imgSaida.setRGB(w, h, novaCor.getRGB());
+                }
+                int resultado = 0;
+                for (int a = 0; a < 9; a++) {
+                    resultado += vizinhanca[a] * kernel[a];
                 }
 
+                if (resultado > 255) resultado = 255;
+                else if (resultado < 0) {
+                    resultado = 0;
+                }
 
+                Color novaCor = new Color(resultado, resultado, resultado);
+
+                imgSaida.setRGB(w, h, novaCor.getRGB());
             }
+
+
         }
-        salvarImagem(imgSaida,"jpg","convolucaoSaida.jpg");
+        salvarImagem(imgSaida,"jpg","convolucaoSaida.png");
         display(imgSaida);
         return imgSaida;
     }
@@ -183,4 +178,67 @@ public class OperacoesLocais {
         frame.pack();
         frame.setVisible(true);
     }
+
+    public static BufferedImage mediaMatrizDefinida(BufferedImage imagem, int tamanhoMatrix) {
+        BufferedImage imgSaida = new BufferedImage(imagem.getWidth(), imagem.getHeight(), imagem.getType());
+            int aux = tamanhoMatrix / 2;
+            for (int h = 0; h < imagem.getHeight(); h++) {
+                for (int w = 0; w < imagem.getWidth(); w++) {
+                    if (w <= aux || h <= aux || w >= imagem.getWidth() - aux || h >= imagem.getHeight() - aux) {
+                        int rgb = imagem.getRGB(w, h);
+                        imgSaida.setRGB(w, h, rgb);
+                        continue;
+                    }
+                    int[] vizinhanca = new int[tamanhoMatrix * tamanhoMatrix];
+                    int count = 0;
+
+                    for (int i = -aux; i <= aux; i++) {
+                        for (int j = -aux; j <= aux; j++) {
+                            Color cor = new Color(imagem.getRGB(w + j, h + i));
+                            vizinhanca[count++] = cor.getRed();
+                        }
+                    }
+                    int media = Arrays.stream(vizinhanca).sum() / vizinhanca.length;
+                    Color novaCor = new Color(media, media, media);
+                    imgSaida.setRGB(w, h, novaCor.getRGB());
+                }
+            }
+
+
+        salvarImagem(imgSaida,"jpg","mediaMatrizValorDefinidoSaida.png");
+        display(imgSaida);
+        return imgSaida;
+    }
+    public static BufferedImage medianaMatrizDefinida(BufferedImage imagem, int tamanhoMatrix) {
+        BufferedImage imgSaida = new BufferedImage(imagem.getWidth(), imagem.getHeight(), imagem.getType());
+         int aux = tamanhoMatrix / 2;
+
+            for (int h = 0; h < imagem.getHeight(); h++) {
+                for (int w = 0; w < imagem.getWidth(); w++) {
+                    if (w <= aux || h <= aux || w >= imagem.getWidth() - aux || h >= imagem.getHeight() - aux) {
+                        int rgb = imagem.getRGB(w, h);
+                        imgSaida.setRGB(w, h, rgb);
+                        continue;
+                    }
+                    int[] vizinhanca = new int[tamanhoMatrix * tamanhoMatrix];
+                    int count = 0;
+                    for (int i = -aux; i <= aux; i++) {
+                        for (int j = -aux; j <= aux; j++) {
+                            Color cor = new Color(imagem.getRGB(w + j, h + i));
+                            vizinhanca[count++] = cor.getRed();
+                        }
+                    }
+                    Arrays.sort(vizinhanca);
+                    int mediana = vizinhanca[tamanhoMatrix * tamanhoMatrix / 2];
+                    Color novaCor = new Color(mediana, mediana, mediana);
+                    imgSaida.setRGB(w, h, novaCor.getRGB());
+                }
+
+            }
+
+        salvarImagem(imgSaida,"jpg","medianaMatrizSaida.png");
+        display(imgSaida);
+        return imgSaida;
+    }
+
 }
